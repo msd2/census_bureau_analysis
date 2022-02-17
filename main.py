4aba74cedf2de27a3c6b896d00b101a64f2b5f8e
msd2 <marc.dunford@gmail.com>
2022-02-17 18:12:31 +0000

    add example Person

--- main.py
+++ main.py
@@ -36,6 +36,25 @@ class Person(BaseModel):
     class Config:
         allow_population_by_field_nane=True
 
+        schema_extra = {
+            'example': {
+                'age': 40,
+                'workclass': 'State-gov',
+                'fnlgt': 77516,
+                'education': 'Bachelors',
+                'education_num': 13,
+                'marital_status': 'Never-married',
+                'occupation': 'Adm-clerical',
+                'relationship': 'Not-in-family',
+                'race': 'White',
+                'sex': 'Male',
+                'capital_gain': 2174,
+                'capital_loss': 0,
+                'hours_per_week': 40,
+                'native_country': 'United-States'
+            }
+        }
+
 
 @app.get("/")
 async def say_hello():
